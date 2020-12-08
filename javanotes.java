7dde89d59f4e8138298278f8363cc76df24af6dc
Reddy12417 <57862240+Reddy12417@users.noreply.github.com>
2020-12-08 09:13:36 +0530

    Add files via upload
    Dec 8th , 2020 notes.

---
+++ javanotes.java
@@ -0,0 +1,12 @@
+Java notes:
+===========
+
+Dec 8th, 2020:
+--------------
+
+- From Java 11 onwards JRE is removed from java software, only JDK.
+- Setting path is for binary files ( /bin foler ).
+- Setting classpath is for library files ( /lib foler ).
+- Setting JAVA_HOME is for tomcat ( JDK folder ).
+- Setting temporary path "set path= C:\Program Files\Java\jdk-15\bin;%path%"
+- In Java 15 , the path is auto set when we install the software.
